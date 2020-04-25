102a2f37bba71f1c3a9f8d139619e800c893835d
aldinoAlfath <64002610+aldinoAlfath@users.noreply.github.com>
2020-04-25 19:47:47 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed = $(".typed");
+
+$(function() {
+  typed.typed({
+    strings: ["Lucky Ardianto.", "Designer.", "Web Designer.", "Freelancer.", " Cyclis"],
+    typeSpeed: 100,
+    loop: true,
+  });
+});
